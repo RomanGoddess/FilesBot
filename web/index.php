2caefbaf7ddcd4bedee312180cbc43469603653b
Utopia <53878781+RomanGoddess@users.noreply.github.com>
2020-10-26 16:00:54 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -90,7 +90,7 @@ function get_link($bot, $chat_id, $message_id) {
         "message_id" => $message_id
     ));
 
-    $required_url = "<b>Here's Your Shareable Link 📃:</b> https://t.me/" . $GLOBALS["TG_BOT_USERNAME"] . "?start=" . "view" . "_" . $req_message->message_id . "_" . "tg"."\n\n<b>Subscribe To Our Premium Apps Channel</b> @Modzilla";
+    $required_url = "<b>Here's Your Shareable Link 📃:</b>\n\nhttps://t.me/" . $GLOBALS["TG_BOT_USERNAME"] . "?start=" . "view" . "_" . $req_message->message_id . "_" . "tg"."\n\n<b>Subscribe To Our Premium Apps Channel</b> @Modzilla";
 
     $bot->api->editMessageText(array(
         "chat_id" => $chat_id,
